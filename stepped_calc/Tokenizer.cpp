2a31a3b35d10b2cb536f58d4b28a9b33a2343d3c
Gil Ho, Jang <jmxexpert@hotmail.com>
2016-03-02 00:08:04 +0900

    implemented tokenizer.

--- stepped_calc/Tokenizer.cpp
+++ stepped_calc/Tokenizer.cpp
@@ -1,10 +1,76 @@
 #include "Tokenizer.h"
 
 
+namespace
+{
+    bool
+    is_operator_char(char c)
+    {
+        switch (c) {
+            case '+':
+            case '-':
+            case '*':
+            case '/':
+                return true;
+
+            default:
+                return false;
+        }
+    }
+
+    template <typename Predicate>
+    std::string
+    take_while(std::string::const_iterator & begin, std::string::const_iterator end, Predicate && p)
+    {
+        std::string s;
+        for ( ; begin != end && p(*begin); ++begin) {
+            s += *begin;
+        }
+        return s;
+    }
+
+    Operator
+    to_operator(char c)
+    {
+        switch (c) {
+            case '+': return Operator::Addition;
+            case '-': return Operator::Subtraction;
+            case '*': return Operator::Multiplication;
+            case '/': return Operator::Division;
+
+            default:
+                break;
+        }
+        throw std::invalid_argument("invalid operator char.");
+    }
+
+    operand_t
+    to_operand(std::string const& s)
+    {
+        return std::stoi(s);
+    }
+} // un-named namespace
+
+
 token_list_t
 tokenize(std::string const& expr)
 {
     token_list_t tokens;
+
+    std::string::const_iterator exprPos = expr.begin();
+    std::string::const_iterator exprEnd = expr.end();
+    std::string strToken;
+
+    while (exprPos != exprEnd) {
+        strToken = take_while(exprPos, exprEnd, [](char c) { return !is_operator_char(c); });
+        if (strToken.empty()) { // if operator or parenthesis,
+            tokens.push_back(to_operator(*exprPos));
+            ++exprPos;
+        } else {
+            tokens.push_back(to_operand(strToken));
+        }
+    }
+
     return tokens;
 }
 
