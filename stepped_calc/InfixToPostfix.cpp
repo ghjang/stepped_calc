c39bc2356812170c7a28fbf57efeb666e6406d83
Gil Ho, Jang <jmxexpert@hotmail.com>
2016-03-02 00:31:22 +0900

    implemented infix_to_postfix function.

--- stepped_calc/InfixToPostfix.cpp
+++ stepped_calc/InfixToPostfix.cpp
@@ -1,10 +1,32 @@
 #include "InfixToPostfix.h"
 
+#include <stack>
+
 
 token_list_t
 infix_to_postfix(token_list_t const& tokens)
 {
     token_list_t tl;
+
+    std::stack<Operator> opStack;
+
+    for (auto & t : tokens) {
+        if (Operator const * pOp = boost::get<Operator>(&t)) {
+            while (!opStack.empty() && opStack.top() >= *pOp) {
+                tl.push_back(opStack.top());
+                opStack.pop();
+            }
+            opStack.push(*pOp);
+            continue;
+        }
+        tl.push_back(t);
+    }
+
+    while (!opStack.empty()) {
+        tl.push_back(opStack.top());
+        opStack.pop();
+    }
+
     return tl;
 }
 
