6478c3746082c4fc9ebc05f9d2cb696215d6b1b8
Gil Ho, Jang <jmxexpert@hotmail.com>
2016-03-11 01:20:27 +0900

    made codes short.

--- stepped_calc/test/OperatorPrecedenceTest.cpp
+++ stepped_calc/test/OperatorPrecedenceTest.cpp
@@ -5,46 +5,52 @@
 
 TEST_CASE("unary operator precedence", "[stepped_calc]")
 {
+    using boost::apply_visitor;
+
     auto const plus = token_t{ UnaryOperator::Plus };
     auto const minus = token_t{ UnaryOperator::Minus };
 
-    REQUIRE(0 == boost::apply_visitor(CompareOperatorPrecedence(), plus, plus));
-    REQUIRE(0 == boost::apply_visitor(CompareOperatorPrecedence(), minus, minus));
-    REQUIRE(0 == boost::apply_visitor(CompareOperatorPrecedence(), plus, minus));
-    REQUIRE(0 == boost::apply_visitor(CompareOperatorPrecedence(), minus, plus));
+    REQUIRE(0 == apply_visitor(CompareOperatorPrecedence(), plus, plus));
+    REQUIRE(0 == apply_visitor(CompareOperatorPrecedence(), minus, minus));
+    REQUIRE(0 == apply_visitor(CompareOperatorPrecedence(), plus, minus));
+    REQUIRE(0 == apply_visitor(CompareOperatorPrecedence(), minus, plus));
 }
 
 TEST_CASE("binary operator precedence", "[stepped_calc]")
 {
+    using boost::apply_visitor;
+
     auto const addition = token_t{ BinaryOperator::Addition };
     auto const subtraction = token_t{ BinaryOperator::Subtraction };
     auto const multiplication = token_t{ BinaryOperator::Multiplication };
     auto const division = token_t{ BinaryOperator::Division };
 
-    REQUIRE(0 == boost::apply_visitor(CompareOperatorPrecedence(), addition, addition));
-    REQUIRE(0 == boost::apply_visitor(CompareOperatorPrecedence(), addition, subtraction));
+    REQUIRE(0 == apply_visitor(CompareOperatorPrecedence(), addition, addition));
+    REQUIRE(0 == apply_visitor(CompareOperatorPrecedence(), addition, subtraction));
 
-    REQUIRE(0 == boost::apply_visitor(CompareOperatorPrecedence(), multiplication, multiplication));
-    REQUIRE(0 == boost::apply_visitor(CompareOperatorPrecedence(), multiplication, division));
+    REQUIRE(0 == apply_visitor(CompareOperatorPrecedence(), multiplication, multiplication));
+    REQUIRE(0 == apply_visitor(CompareOperatorPrecedence(), multiplication, division));
 
-    REQUIRE(1 == boost::apply_visitor(CompareOperatorPrecedence(), addition, multiplication));
-    REQUIRE(1 == boost::apply_visitor(CompareOperatorPrecedence(), addition, division));
+    REQUIRE(1 == apply_visitor(CompareOperatorPrecedence(), addition, multiplication));
+    REQUIRE(1 == apply_visitor(CompareOperatorPrecedence(), addition, division));
 
-    REQUIRE(-1 == boost::apply_visitor(CompareOperatorPrecedence(), multiplication, addition));
-    REQUIRE(-1 == boost::apply_visitor(CompareOperatorPrecedence(), division, addition));
+    REQUIRE(-1 == apply_visitor(CompareOperatorPrecedence(), multiplication, addition));
+    REQUIRE(-1 == apply_visitor(CompareOperatorPrecedence(), division, addition));
 }
 
 TEST_CASE("operator precedence", "[stepped_calc]")
 {
+    using boost::apply_visitor;
+
     auto const plus = token_t{ UnaryOperator::Plus };
     auto const minus = token_t{ UnaryOperator::Minus };
 
     auto const addition = token_t{ BinaryOperator::Addition };
     auto const multiplication = token_t{ BinaryOperator::Multiplication };
 
-    REQUIRE(-1 == boost::apply_visitor(CompareOperatorPrecedence(), plus, addition));
-    REQUIRE(-1 == boost::apply_visitor(CompareOperatorPrecedence(), minus, multiplication));
-    REQUIRE(1 == boost::apply_visitor(CompareOperatorPrecedence(), addition, plus));
-    REQUIRE(1 == boost::apply_visitor(CompareOperatorPrecedence(), multiplication, minus));
+    REQUIRE(-1 == apply_visitor(CompareOperatorPrecedence(), plus, addition));
+    REQUIRE(-1 == apply_visitor(CompareOperatorPrecedence(), minus, multiplication));
+    REQUIRE(1 == apply_visitor(CompareOperatorPrecedence(), addition, plus));
+    REQUIRE(1 == apply_visitor(CompareOperatorPrecedence(), multiplication, minus));
 }
 
